5d9d98e14fa2542a7a57b45fce263d41a445b980
mayks1 <mayks1@gmail.com>
2020-04-05 08:10:26 +0300

    host: 'https://nuxt-unubo.unubo.app/'

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   */
  server: {
     port: process.env.PORT || 3000, // default: 3000
-    // host: '0.0.0.0' // default: localhost
+    host: 'https://nuxt-unubo.unubo.app/' // default: localhost
   },
 
   build: {
